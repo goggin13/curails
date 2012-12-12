7218123be3ec729047986d46d25f23c9a4874aa6
Matt Goggin <goggin13@gmail.com>
2012-12-11 21:38:12 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curails::Application.config.secret_token = '977da7fd9b15f61611adbe9e0ed642c150a7bb6c6100db1b5b6b71053a9c894c0d8f5efbf642489f1c637b584bba19da7bd939517ac5f6e31cce471a018d8659'
